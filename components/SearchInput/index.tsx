7eab18dbe06e2b4d3d223257b6b69dcc8ec039d2
filipepl82 <filipe.pl82@gmail.com>
2022-06-01 04:02:29 -0300

    Inicialização do Projeto, desenvolvimento do Front-End

---
+++ components/SearchInput/index.tsx
@@ -0,0 +1,44 @@
+import { KeyboardEvent, useState } from 'react';
+import styles from './styles.module.css';
+
+type Props = {
+  mainColor: string;
+  onSearch: (searchValue: string) => void;
+}
+
+export const SearchInput = ({mainColor, onSearch}: Props) => {
+  
+  const [focused, setFocused] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  
+  const HandleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.code === 'Enter') {
+      onSearch(searchValue);
+    }
+  }
+
+  const handleKeyUp = () => {
+    onSearch(searchValue);
+  }
+
+  return (
+    <div 
+      className={styles.container} 
+      style={{borderColor: focused ? mainColor : '#FFF'}}>
+      <div 
+        className={styles.button}
+        onClick={() => onSearch(searchValue)}
+      ></div>
+      <input 
+        type="text" 
+        className={styles.input}
+        placeholder="Digite o nome do produto"
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onKeyUp={handleKeyUp}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+    </div>
+  );
+}
